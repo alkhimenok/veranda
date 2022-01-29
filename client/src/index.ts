f00b7921fa1ad6b3f6730b7123915e7f781492cc
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-01-30 00:13:59 +0300

    refactor: header

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,3 @@
 document.querySelector('#burgerIcon')?.addEventListener('click', () => document.querySelector('#header')?.classList.toggle('_show-menu'))
 
-document.querySelector('main')!.style.marginTop = getComputedStyle(document.querySelector('header')!).height
+// document.querySelector('main')!.style.marginTop = getComputedStyle(document.querySelector('header')!).height
